205610ac713fe3161e64bf49564386cd453fab15
RolandSung <mr.songxr@gmail.comgit config --global user.name RolandSung>
2016-11-04 17:40:51 +0800

    添加CZAdditions文件 通过桥接文件让Swift引用OC代码

--- weiBo-Swift3.0/weiBo-Swift3.0/AppDelegate.swift
+++ weiBo-Swift3.0/weiBo-Swift3.0/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
-        window?.backgroundColor = UIColor.white
+        window?.backgroundColor = UIColor.cz_random()
         
         window?.rootViewController = ASMainViewController()
         
